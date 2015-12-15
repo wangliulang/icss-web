c3a546a0c6fcc0d7316c3ca812b234813bbab00a
wangliulang <liulangwang@aliyun.com>
2015-12-15 22:14:30 +0800

    添加spring mvc 上传 添加mybatis mysql blob类型字段插入的示例

--- Test/src-biz/src/com/icss/controller/TestController.java
+++ Test/src-biz/src/com/icss/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.icss.controller;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -7,6 +8,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -19,6 +21,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 import com.icss.framework.controller.BaseController;
 
@@ -79,5 +84,59 @@ public class TestController extends BaseController{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	@RequestMapping(value = "/blob")
+    public void testblob(HttpServletRequest request, HttpServletResponse response) {
+		//创建一个通用的多部分解析器  
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver(request.getSession().getServletContext());  
+        //判断 request 是否有文件上传,即多部分请求  
+        if(multipartResolver.isMultipart(request)){  
+            //转换成多部分request    
+            MultipartHttpServletRequest multiRequest = (MultipartHttpServletRequest)request;  
+            //取得request中的所有文件名  
+            Iterator<String> iter = multiRequest.getFileNames();  
+            while(iter.hasNext()){  
+                //记录上传过程起始时的时间，用来计算上传时间  
+                int pre = (int) System.currentTimeMillis();  
+                //取得上传文件  
+                MultipartFile file = multiRequest.getFile(iter.next());  
+                if(file != null){  
+                    //取得当前上传文件的文件名称  
+                    String myFileName = file.getOriginalFilename();
+                    //如果名称不为“”,说明该文件存在，否则说明该文件不存在  
+                    if(myFileName.trim() !="") {
+                    	Map param = new HashMap();
+                        byte[] b;
+						try {
+							b = file.getBytes();
+							if (b == null || b.length == 0) { 
+
+	                        }else {
+	                        	param.put("testblob", b);
+	                        	this.baseService.insertOne("test.blob.insertBlob", param);
+	                        }
+						} catch (IOException e1) {
+							e1.printStackTrace();
+						}
+                        /*System.out.println(myFileName);  
+                        //重命名上传后的文件名  
+                        String fileName = "demoUpload" + file.getOriginalFilename();  
+                        //定义上传路径  
+                        String path = "D:/" + fileName;  
+                        File localFile = new File(path);  
+                        try {
+							file.transferTo(localFile);
+						} catch (IllegalStateException | IOException e) {
+							e.printStackTrace();
+						}  */
+                    }  
+                }  
+                //记录上传该文件后的时间  
+                int finaltime = (int) System.currentTimeMillis();  
+                System.out.println(finaltime - pre);  
+            }  
+              
+        }  
 	}
 }
